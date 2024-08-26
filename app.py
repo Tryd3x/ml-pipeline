3205e904746d19c6b05844bce88c639a779af8cd
Trydex <hyderrezatelegraphy@gmail.com>
2024-08-25 20:08:44 -0500

    Added Exception handler

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from src.logger import logging
+from src.exception import CustomException
+import sys
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    logging.info('User accessed the homepage')
-    return "Hello, world!"
+    try:
+        logging.info('User tried to access the homepage')
+        raise Exception("Testing CustomException handler")
+    except Exception as e:
+        logging.info(CustomException(e,sys).error_message)
+        return "Hello! World!"
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ src/exception.py
@@ -0,0 +1,26 @@
+import sys
+from src.logger import logging
+
+def error_message_detailed(error, error_detailed: sys):
+    exc_type,exc_val,exc_traceback = error_detailed.exc_info()
+
+    file_name = exc_traceback.tb_frame.f_code.co_filename
+    error_message = f"Error occured in python script name [{file_name}] line number [{exc_traceback.tb_lineno}] error message [{error}]"
+    
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detailed: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detailed(error_message, error_detailed = error_detailed)
+
+    def __str__(self):
+        return self.error_message
+    
+# if __name__ == "__main__":
+#     try:
+#         a = 1/0
+#     except Exception as e:
+#         print("Entered exception block")
+#         logging.error("division by zero")
+#         raise CustomException(e,sys)
